2363afaafe23a87d58984babc7dd51729d179150
BearPro <bearpro@outlook.com>
2019-08-29 20:14:06 +0300

    Контроллер главной страницы теперь возвращает тупо все соревнования.

--- Rauthor/Controllers/HomeController.cs
+++ Rauthor/Controllers/HomeController.cs
@@ -16,22 +16,8 @@ namespace Rauthor.Controllers
         }
         public IActionResult Index()
         {
-            var closing = database.Competitions
-                .Include("Participants")
-                .Where(c => c.StartDate > DateTime.Now)
-                .OrderByDescending(c => c.StartDate)
-                .Take(2);
-            var resulting = database.Competitions
-                .Include("Participants")
-                .Where(c => c.StartDate < DateTime.Now && DateTime.Now < c.EndDate)
-                .OrderBy(c => c.EndDate)
-                .Take(2);
-            var model = new RelevantCompetitionsModel()
-            {
-                ClosingApplyCompetitions = closing,
-                IncomingResultCompetitions = resulting
-            };
-            return View(model);
+            var competition = database.Competitions.Include("Participants");
+            return View(competition);
         }
         public IActionResult Privacy()
         {
