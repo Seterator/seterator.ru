adf51a6ebb818a1184a94d5c2191b090694043cf
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-10-09 17:59:49 +0300

    Обновлен дизайн страницы регистрации. Обновлен фон хедера. Уменьшена иконка в футере.

--- Rauthor/wwwroot/js/account.ts
+++ Rauthor/wwwroot/js/account.ts
@@ -1,12 +1,18 @@
 ﻿function switchForm(): void {
     let loginView = document.getElementById("login-form");
     let registerView = document.getElementById("register-form");
+    let authButton = document.getElementById("account-register-button");
+    let registerButton = document.getElementById("account-auth-button");
     if (loginView.style.display == "block") {
         loginView.style.display = "none";
         registerView.style.display = "block"
+        authButton.style.background = "rgb(255, 57, 0)"
+        registerButton.style.background = "rgb(65, 65, 65)"
     }
     else {
         loginView.style.display = "block"
         registerView.style.display = "none";
+        authButton.style.background = "rgb(65, 65, 65)"
+        registerButton.style.background = "rgb(255, 57, 0)"
     }
 }
