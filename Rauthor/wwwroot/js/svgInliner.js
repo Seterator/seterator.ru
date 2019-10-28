4466c93dfd1bec94abcab529de895e2fa96e4e67
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-10-28 19:49:14 +0300

    Изменен цвет ссылок на оранжевый. Также добавлен скрипт заинлайнивания svg картинок, чтобы применять к ним стили. Изменен цвет svg картинок.

---
+++ Rauthor/wwwroot/js/svgInliner.js
@@ -0,0 +1,16 @@
+﻿$('img.img-svg').each(function () {
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    $.get(imgURL, function (data) {
+        var $svg = $(data).find('svg');
+        if (typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass + ' replaced-svg');
+        }
+        $svg = $svg.removeAttr('xmlns:a');
+        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+        $img.replaceWith($svg);
+    }, 'xml');
+});
