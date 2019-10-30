e33d2743bee3cdce23afb8ce06612178789289a1
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-10-30 15:42:33 +0300

    Добавлен блок успешной регистрации. Изменен размер картинки на регистрации.

--- Rauthor/wwwroot/js/account.ts
+++ Rauthor/wwwroot/js/account.ts
@@ -36,3 +36,26 @@
         authButton.style.color = "rgb(132, 132, 132)";
     }
 }
+
+function successReg(): void {
+    let authContainer = document.getElementById("auth");
+    let sucView = document.getElementById("successReg");
+    let authCheersView = document.getElementById("auth-cheers");
+    let regCheersView = document.getElementById("reg-cheers");
+    let authFormContainerView = document.getElementById("auth-form-container");
+    
+    let regView = document.getElementById("register-form");
+
+    authContainer.style.gridTemplateColumns = "0.33fr";
+    authContainer.style.alignContent = "center";
+    authContainer.style.justifyContent = "center";
+
+    regView.style.display = "none";
+    authCheersView.style.display = "none";
+    regCheersView.style.display = "none";
+    authFormContainerView.style.display = "none";
+    
+
+
+    sucView.style.display = "flex";
+}
