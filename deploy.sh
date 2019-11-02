e8ba71a8297e894bb3fd3435bb88a1b73799f1f1
BearPro <bearpro@outlook.com>
2019-11-02 20:35:50 +0300

    Настроено автоматическое развёртывание на сервере.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo "Deployment started."
+sshpass -e -v ssh -o StrictHostKeyChecking=no root@5.63.154.249 '
+    echo "SSH session started.";
+    cd seterator;
+    systemctl stop seterator;
+    echo "Service stopped."
+    git pull;
+    dotnet build;
+    echo "Build completed."
+    systemctl start seterator;
+    echo "Service started.";
+'
+echo "Deployment complited."
