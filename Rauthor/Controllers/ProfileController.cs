7e763fe608e6597b0ff3d1e9d3a9c4e598cebbab
BearPro <bearpro@outlook.com>
2019-11-22 11:53:38 +0300

    Добавлены ссылки на профили пользователей. Добавлены короткие ссылки на профили пользователей. Просмотр профиля теперь доступен не авторизованным пользователям.

--- Rauthor/Controllers/ProfileController.cs
+++ Rauthor/Controllers/ProfileController.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 namespace Rauthor.Controllers
 {
-    [Authorize(Roles = "User")]
     public class ProfileController : Controller
     {
         DatabaseContext database;
@@ -18,12 +17,46 @@ namespace Rauthor.Controllers
         {
             this.database = database;
         }
-        [Authorize]
+        [Authorize(Roles = "User")]
         public IActionResult Index()
         {
             User user = HttpContext.Session.Get<User>("user");
             var profile = ProfileModel.ReadFromDatabase(user.Guid, database);
             return View(profile);
         }
+
+        [HttpGet("[controller]/{shortLink}")]
+        public IActionResult Profile(string shortLink)
+        {
+            try
+            {
+                Guid user_guid = database.Profiles
+                    .Join(database.Roles, profile => profile.RoleGuid, role => role.Guid, (profile, role) => new { profile, role })
+                    .Join(database.Users, t => t.role.UserGuid, user => user.Guid, (t, user) => new { t.profile, t.role, user })
+                    .Single(x => x.profile.ShortLink == shortLink)
+                    .user.Guid;
+                var profile = ProfileModel.ReadFromDatabase(user_guid, database);
+                return View("Index", profile);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+            
+        }
+        [HttpGet("[controller]/{guid}")]
+        public IActionResult Profile(Guid guid)
+        {
+            try
+            {
+                var profile = ProfileModel.ReadFromDatabase(guid, database);
+                return View("Index", profile);
+            }
+            catch (NullReferenceException e)
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
