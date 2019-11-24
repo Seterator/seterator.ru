5534e72cfd011058c3ef0a5dc19b430bf2b56f75
BearPro <bearpro@outlook.com>
2019-11-24 16:22:58 +0300

    Выполнен рефакторинг тестов Entity Framework.

---
+++ Rauthor.UnitTests/Database/DatabaseTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+
+namespace Rauthor.UnitTests.Database
+{
+    public class DatabaseTest
+    {
+        protected IConfiguration Config => new ConfigurationBuilder()
+            .AddUserSecrets<Startup>()
+            .Build();
+        protected DbContextOptions<DatabaseContext> Options => new DbContextOptionsBuilder<DatabaseContext>()
+            .UseMySQL(Config.GetConnectionString("Local MySQL")).Options;
+        protected IMemoryCache Cache => null;
+        protected DatabaseContext Database => new DatabaseContext(Options, Config, Cache);
+    }
+}
