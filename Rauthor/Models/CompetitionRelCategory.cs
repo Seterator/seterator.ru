fca6290c1a88e8358a0190f8270b88b77f564829
BearPro <bearpro@outlook.com>
2019-11-24 16:06:28 +0300

    Исправлены ошибки в моделях сущностей.

--- Rauthor/Models/CompetitionRelCategory.cs
+++ Rauthor/Models/CompetitionRelCategory.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Rauthor.Models
 {
+    [Table("competition_rel_category")]
     public class CompetitionRelCategory
     {
+        [Column("competition_category_guid")]
         public Guid CompetitionGuid { get; set; }
+        
+        [Column("competition_guid")]
         public Guid CategoryGuid { get; set; }
 
         [ForeignKey("CompetitionGuid")]
