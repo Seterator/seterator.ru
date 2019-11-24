876e0c2131a1aeab5126fe51b87f35e12bfb9b95
BearPro <bearpro@outlook.com>
2019-11-24 23:18:25 +0300

    Добавлены тесты для api-контроллера AccountController.

---
+++ Rauthor.UnitTests/Controllers/Api/Account.cs
@@ -0,0 +1,35 @@
+﻿using Rauthor.Models.Api;
+using System.Linq;
+using Xunit;
+
+namespace Rauthor.UnitTests.Controllers.Api
+{
+    public class Account
+    {
+        [Fact]
+        public void RegisterCorrect()
+        {
+            Database.ConfigureInstanceAndData();
+            using (var db = Database.Instance)
+            {
+                var controller = new Rauthor.Controllers.Api.AccountController(db);
+                var result = controller.Post(new Login() { Method = "register", Password = "password", Username = "sample_user" });
+                Assert.Equal(result.Result, "accept");
+                db.Users.Single(x => x.Login == "sample_user");
+            }
+        }
+
+        [Fact]
+        public void LoginCorrect()
+        {
+            Database.ConfigureInstanceAndData();
+            using (var db = Database.Instance)
+            {
+                var controller = new Rauthor.Controllers.Api.AccountController(db);
+                controller.Post(new Login() { Method = "register", Password = "password", Username = "sample_user" });
+                var result = controller.Post(new Login() { Method = "login", Password = "password", Username = "sample_user" });
+                Assert.Equal(result.Result, "accept");
+            }
+        }
+    }
+}
