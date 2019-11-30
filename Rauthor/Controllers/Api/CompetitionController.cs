a46b8662d6a15356a230bec7ca8ad45da88c9860
Михаил Проказин <bearpro@outlook.com>
2019-11-30 13:18:52 +0300

    Api-контроллер для соревнований (#55)
    * Добавлена модель полных данных о соревновании для использования в api.
    
    * Обновлён action, отвечающий за создание нового соревнования.
    Теперь в нём используется новая модель.
    
    * Добавлена модель полных данных о соревновании для использования в api.
    
    * Обновлён action, отвечающий за создание нового соревнования.
    Теперь в нём используется новая модель.
    
    * Добавлен статический метод, возвращающий экземпляр Competition на основе ViewModels.Api.Competition.
    
    * Обновлён метод, обрабатывающий создание соревнования. Добавлен метод, обрабатывающий. изменение конкурса.
    
    * Добавлен конструктор по умолчанию, инициализирующий пустые списки для навигационных свойств.
    
    * Обновлён конструктор по умолчанию.
    
    * Исправлены неправильные настройки прав доступа.
    Пользователи теперь могут просматривать информацию о соревновании до авторизации.
    Методы контроллера теперь возвращают OkObjectResult через IActionResult.
    
    * Подавлена ошибка "Идентификатор содержит имя типа". Использовать 'Guid guid' слишком удобно.
    
    * Исправлена модель представления для api-контроллера Competition.

--- Rauthor/Controllers/Api/CompetitionController.cs
+++ Rauthor/Controllers/Api/CompetitionController.cs
@@ -21,39 +21,38 @@ namespace Rauthor.Controllers.Api
             database = db;
         }
 
-        [Authorize(Roles = "User")]
         [HttpGet]
-        public IEnumerable<Competition> Get()
+        public IActionResult Get()
         {
-            return database.Competitions;
+            return Ok(database.Competitions);
         }
 
-        [Authorize(Roles = "User")]
         [HttpGet("{guid}", Name = "Get")]
-        public string Get(Guid guid)
+        public IActionResult Get(Guid guid)
         {
             var value = database.Competitions.Include(x => x.Participants).Where(x => x.Guid == guid).Single();
             value.Participants = value.Participants.Select(x => { x.Competition = null; return x; }).ToList();
-            return Newtonsoft.Json.JsonConvert.SerializeObject(value);
+            return Ok(value);
         }
 
         [Authorize(Roles = "Admin, Manager")]
         [HttpPost]
-        public IActionResult Post([FromBody] Competition value)
+        public IActionResult Post([FromBody] ViewModels.Api.Competition value)
         {
-            value.Guid = Guid.NewGuid();
-            database.Competitions.Add(value);
+            var competition = Competition.FromApiViewModel(value);
+            competition.Guid = Guid.NewGuid();
+            database.Competitions.Add(competition);
             database.SaveChanges();
             return Ok();
         }
         
         [Authorize(Roles = "Admin, Manager")]
         [HttpPut("{guid}")]
-        public IActionResult Put(Guid guid, [FromBody] Competition value)
+        public IActionResult Put(Guid guid, [FromBody] ViewModels.Api.Competition value)
         {
             var oldValue = database.Competitions.Where(x => x.Guid == guid).Single();
             value.Guid = guid;
-            database.Entry(oldValue).CurrentValues.SetValues(value);
+            database.Entry<Competition>(oldValue).CurrentValues.SetValues(Competition.FromApiViewModel(value));
             database.SaveChanges();
             return Ok();
         }
---
+++ Rauthor/ViewModels/Api/Competition.cs
@@ -0,0 +1,92 @@
+﻿using Rauthor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rauthor.ViewModels.Api
+{
+    /// <summary>
+    /// Модель используемая api-контроллером соревнований.
+    /// </summary>
+    public class Competition
+    {
+        /// <summary>
+        /// Guid конкурса
+        /// </summary>
+        public Guid? Guid { get; set; }
+
+        /// <summary>
+        /// Номер телефона организатора
+        /// </summary>
+        public string ManagerPhoneNumber { get; set; }
+
+        /// <summary>
+        /// Электронная почта организатора
+        /// </summary>
+        public string ManagerEmail { get; set; }
+        
+        /// <summary>
+        /// Социальные сети организатора
+        /// </summary>
+        public List<string> ManagerSocialLinks { get; set; }
+
+        /// <summary>
+        /// Категория конкурса
+        /// </summary>
+        public List<Guid> Categories { get; set; }
+
+        /// <summary>
+        /// НАзвание конкурса
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Короткое описание конкурса
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// Полное описание конкурса
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Условия конкурса
+        /// </summary>
+        public List<CompetitionConstraint> Constraints { get; set; }
+
+        /// <summary>
+        /// Призовые места
+        /// </summary>
+        public object Prizes { get; set; }
+
+        /// <summary>
+        /// Guid'ы жюри конкурса
+        /// </summary>
+        public List<Guid> JuryGuids { get; set; }
+
+        /// <summary>
+        /// Дата публикации конкурса
+        /// </summary>
+        public DateTime PublicationDate { get; set; }
+
+        /// <summary>
+        /// Дата начала приёма заявок
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Дата окончания приёма заявок
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        public Competition()
+        {
+            JuryGuids = new List<Guid>();
+            Constraints = new List<CompetitionConstraint>();
+            Categories = new List<Guid>();
+            ManagerSocialLinks = new List<string>();
+        }
+    }
+}
