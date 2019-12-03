8c95a572f5e20c18c44b6139133232060c91893d
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-12-03 23:23:53 +0300

    Добавлен вывод конкурсов с помощью fetch API на главной странице.

--- Rauthor/wwwroot/js/Competition/Timer.js
+++ Rauthor/wwwroot/js/Competition/Timer.js
@@ -1,3 +1,13 @@
+function ReturnTime(competitionDate) {
+    var difference = new Date(competitionDate) - Date.now();
+        
+    var days = Math.trunc(difference / (1000 * 3600 * 24));
+    var hours = Math.trunc(difference / (1000 * 3600)) - days * 24;
+    var min = Math.trunc(difference / (1000 * 60)) - days * 24 * 60 - hours * 60;
+    var answ = days + " д " + hours + " ч " + min + " м";
+    return answ;
+}
+
 window.fetch(`/api/competition/${guid}`).then(response => response.json()).then(competition => {
     setInterval(changeTime, 1000);
     
