4a64fde47471097884960bdf0af25333f5f49ed9
BearPro <bearpro@outlook.com>
2019-12-04 16:11:18 +0300

    Добавлена загрузка связанных сущностей в api-контроллер соревнования.

---
+++ Rauthor/Controllers/Api/ParticipantController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rauthor.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantController
+    {
+
+    }
+}
