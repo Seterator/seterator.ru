02779e89c63bf60ed6bf622d766f6194b243156b
BearPro <bearpro@outlook.com>
2019-12-18 02:11:14 +0300

    Добавлен контроллер для отладки маршрутов.

---
+++ Rauthor/Controllers/Api/RoutesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Rauthor.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
+
+        public RoutesController(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
+        {
+            this._actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var routes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.Select(x => new {
+                Action = x.RouteValues["Action"],
+                Controller = x.RouteValues["Controller"],
+                Name = x.AttributeRouteInfo?.Name,
+                Template = x.AttributeRouteInfo?.Template,
+                Constraints = x.ActionConstraints
+            }).ToList();
+            return Ok(routes);
+        }
+    }
+}
