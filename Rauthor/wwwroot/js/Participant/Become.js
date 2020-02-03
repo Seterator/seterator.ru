c3e5762179dcf2f6b10cb611f99e86250be9cfe6
semen1032 <51064226+semen1032@users.noreply.github.com>
2020-02-03 13:43:35 +0300

    Описан запрос на подачу заявки.

--- Rauthor/wwwroot/js/Participant/Become.js
+++ Rauthor/wwwroot/js/Participant/Become.js
@@ -3,6 +3,9 @@ fetch(`/api/Competition/${guid}`, {
     method: 'GET'
 }).then(response => response.json()).then(competition => LoadCompetition(competition));
 
+let participantForm = document.forms[0];
+participantForm.addEventListener("submit", SendForm);
+
 function LoadCompetition(competition) {
     LoadTimer();
     LoadCompetitionDetails();
@@ -17,4 +20,28 @@ function LoadCompetition(competition) {
         document.querySelector(".competitionItem__title").textContent = competition.title;
         document.querySelector(".competitionItem__description").textContent = competition.description;
     }
+}
+
+function SendForm() {
+    event.preventDefault();
+    let participantForm = document.forms[0];
+
+    let application = {
+        title: participantForm.title.value,
+        nickname: participantForm.nickname.value,
+        poem: participantForm.poem.value
+    }
+
+    fetch("/api/Participant", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "text/json; charset=utf-8"
+        },
+        body: JSON.stringify(application)
+    }).then(response => {
+        if (response.status == 200)
+            document.location.href = "/";
+        else
+            alert(`Ошибка: ${response.status}`);
+    });
 }
