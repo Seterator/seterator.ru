2007e06b029df644c4951f70871810c656254c92
semen1032 <51064226+semen1032@users.noreply.github.com>
2020-02-09 13:14:12 +0300

    Добавлены компоненты constraints-component и element-of-constraints-component.

--- Rauthor/wwwroot/js/Competition/Details.js
+++ Rauthor/wwwroot/js/Competition/Details.js
@@ -68,12 +68,73 @@ document.addEventListener('DOMContentLoaded', function(){
             };
         }
     };
+
+    const ElementOfConstraintsComponent = {
+        props: [
+            'constraintElement'
+        ],
+        template: `
+            <li>
+                <div class="competitionConditions__item competitionConditions__item_marker_orangeDot"
+                    v-if="constraintElement.type == 'basic'"> 
+                    <span class="competitionConditions__name">
+                        {{constraintElement.name}}
+                    </span> 
+                    <span class="competitionConditions__value">
+                        {{constraintElement.value}}
+                    </span>
+                </div>
+                <div class="competitionConditions__item competitionConditions__item_marker_orangeDot"
+                    v-else-if="constraintElement.type == 'line'"> 
+                    <span class="competitionConditions__name">
+                        {{constraintElement.name}}
+                    </span>
+                </div>
+                <div class="competitionConditions__item competitionConditions__item_type_file"
+                    v-else-if="constraintElement.type == 'file'">
+                    <div class="imgText">
+                        <img class="imgText__img imgSvg imgSvg_color_orange" src="/img/Competition/link_1.svg"
+                            v-if="constraintElement.name == 'Условия конкурса'">
+                        <img class="imgText__img imgSvg imgSvg_color_orange" src="/img/Competition/document.svg"
+                            v-else>
+                        <span class="competitionConditions__fileName">{{constraintElement.name}}</span>
+                    </div>
+                </div>
+            </li>
+        `
+    };
+
+    const ConstraintsComponent = {
+        props: [
+            'constraints'
+        ],
+        template: `
+            <div>
+                <div class="competitionDetails__competitionConditions">
+                    <h3 class="competitionDetails__header">
+                        {{title}}
+                    </h3>
+                    <ul class="competitionConditions">
+                        <element-of-constraints-component :constraintElement="constraintElement" :key="constraintElement.name" v-for="constraintElement of this.constraints"></element-of-constraints-component>
+                    </ul>
+                </div>
+            </div>
+        `,
+        data() {
+            return {
+                title: 'Условия конкурса'
+            };
+        },
+        components: {
+            'element-of-constraints-component': ElementOfConstraintsComponent
+        }
+    };
     
     let vm = new Vue({
         el: '.app',
         components: {
             'banner-component': BannerComponent,
-            'constraints-component': ConstraintsComponent
+            'constraints-component': ConstraintsComponent,
         },
         data: {
             competition: {}
@@ -93,3 +154,160 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 });
+
+
+
+// HideElems();
+
+// var guid = window.location.pathname.split(new RegExp("/|\\?"))[3];
+// fetch(`/api/Competition/${guid}`, {
+//     method: 'GET'
+// }).then(response => response.json()).then(competition => LoadCompetition(competition));
+
+// function LoadCompetition(competition) {
+//     UnhideElems();
+//     LoadCompetitionBanner();
+//     LoadCompetitionDescription();
+//     LoadCompetitionConditions();
+//     LoadCompetitionPrizes();
+//     LoadCompetitionJury();
+//     LoadCompetitionParticipants();
+
+//     function LoadCompetitionBanner() {
+//         LoadOrganizerInfo();
+//         LoadCompetitionBannerInfo();
+        
+//         function LoadOrganizerInfo(competition) {
+//             document.querySelector(".parsonalInfo__name").textContent = "Не указано";
+//             document.querySelector(".personalInfo__phone").textContent = "Не указано";
+//             document.querySelector(".personalInfo__email").textContent = "Не указано";
+
+//         }
+//         function LoadCompetitionBannerInfo() {
+//             document.querySelector(".competitionItem__category").textContent = "Ежедневный конкурс";
+//             document.querySelector(".competitionItem__title").textContent = competition.title;
+//             document.querySelector(".competitionItem__shortDescription").textContent = competition.shortDesctiption;
+//             setInterval(() => {
+//                 document.querySelector(".competitionItem__timer").textContent = ReturnTime(competition.endDate);
+//             }, 1000);
+
+//         }
+//     }
+//     function LoadCompetitionDescription() {
+//         document.querySelector(".competitionItem__fullDescription").textContent = competition.description;
+//     }
+//     function LoadCompetitionConditions() {
+//         document.querySelector(".competitionConditions").textContent = "Временно недоступно. В разработке.";
+//     }
+//     function LoadCompetitionPrizes() {
+//         LoadCompetitionPrizesLit();
+
+//         function LoadCompetitionPrizesLit() {
+//             competition.prizes.forEach(prize => document.querySelector(".competitionPrizes").append(CreateCompetitionPrize(prize)));
+//         }
+//         function CreateCompetitionPrize(prize) {
+//             var prizeItem = document.createElement("li");
+//             prizeItem.classList.add("competitionPrizes__item");
+
+//             var prizeItemPlace = document.createElement("span");
+//             prizeItemPlace.classList.add("competitionPrizes__name");
+//             prizeItemPlace.textContent = `${prize.begin_place} - ${prize.end_place} место: `;
+
+//             var prizeItemValue = document.createElement("span");
+//             prizeItemValue.classList.add("competitionPrizes__value");
+//             prizeItemValue.textContent = prize.value;
+
+//             prizeItem.append(prizeItemPlace, prizeItemValue);
+//             return prizeItem;
+//         }
+//     }
+//     function LoadCompetitionJury() {
+//         LoadJuryCount();
+//         CreateJuryList();
+
+//         function LoadJuryCount() {
+//             document.querySelector(".jury__count").textContent = `(${competition.jury.length})`;
+//         }
+//         function CreateJuryList() {
+//             if (competition.jury.length != 0) {
+//                 document.querySelector(".jury__showAllBtn").style.removeProperty("display");
+//                 competition.jury.forEach(juryItem => {
+//                     document.querySelector(".jury__lst").append(CreateJuryItem(juryItem))
+//                 });
+//             }
+//             function CreateJuryItem(juryJSON) {
+//                 var juryItemLi = document.createElement("li");
+//                 juryItemLi.classList.add("userItem");
+
+//                 var imgText = document.createElement("div");
+//                 imgText.classList.add("imgText");
+
+//                 var avatar = document.createElement("div");
+//                 avatar.classList.add("avatar", "avatar_shape_circle", "avatar_theme_dark", "avatar_size_m");
+
+//                 var juryName = document.createElement("span");
+//                 juryName.classList.add("userItem__name");
+//                 juryName.textContent = "Имя жюри"; //juryJSON.nickname
+
+//                 var juryDescription = document.createElement("p");
+//                 juryDescription.classList.add("userItem__description");
+//                 juryDescription.textContent = "Описание жюри"; //juryJSON.description
+
+//                 imgText.append(avatar, juryName);
+//                 juryItemLi.append(imgText, juryDescription);
+
+//                 return juryItemLi;
+//             }
+//         }
+//     }
+//     function LoadCompetitionParticipants() {
+//         LoadCompetitionParticipantsCount();
+//         LoadCompetitionParticipantsList();
+
+//         function LoadCompetitionParticipantsCount() {
+//             document.querySelector(".participants__count").textContent = `(${competition.participants.length})`;
+//         }
+//         function LoadCompetitionParticipantsList() {
+//             competition.participants.forEach(participant => document.querySelector(".participants__lst").append(CreateCompetitionParticipant(participant)));
+
+//             function CreateCompetitionParticipant(participant) {
+//                 var participantItem = document.createElement("li");
+//                 participantItem.classList.add("participantItem");
+
+//                 var poemTitle = document.createElement("span");
+//                 poemTitle.classList.add("participantItem__poemTitle");
+//                 poemTitle.textContent = participant.title;
+                
+//                 var nickname = document.createElement("span");
+//                 nickname.classList.add("participantItem__nickname");
+//                 nickname.textContent = participant.nickname;
+
+//                 participantItem.append(poemTitle, nickname);
+//                 return participantItem;
+//             }
+//         }
+//         function ShowCompetitionParticipantsBtn() {
+//             if (competition.participants.length != 0) {
+//                 document.querySelector(".participants__showAllBtn").style.removeProperty("display");   
+//             }
+//         }
+//     }
+
+// }
+
+// function HideElems() {
+//     // Информация об организаторе
+//     document.querySelector(".personalInfo__avatar").style.display = "none";
+//     document.querySelector(".personalInfo__phone").parentElement.style.display = "none";
+//     document.querySelector(".personalInfo__email").parentElement.style.display = "none";    
+//     document.querySelector(".jury__showAllBtn").style.display = "none";    
+//     document.querySelector(".participants__showAllBtn").style.display = "none";   
+//     document.querySelectorAll(".competitionConditions__item").forEach(condition => condition.style.display = "none");
+    
+// }
+
+// function UnhideElems() {
+//     document.querySelector(".personalInfo__avatar").style.removeProperty("display");
+//     document.querySelector(".personalInfo__phone").parentElement.style.removeProperty("display");
+//     document.querySelector(".personalInfo__email").parentElement.style.removeProperty("display");
+// }
