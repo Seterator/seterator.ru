cfa3e2a07117994974042828997d67d4dfd140a5
BearPro <bearpro@outlook.com>
2020-02-17 10:55:56 +0000

    Добавлен новый контроллер для создания заявки на участие в конкурсе. Согласованы модели для этого контроллера.

--- Rauthor/Controllers/Api/ParticipantController.cs
+++ Rauthor/Controllers/Api/ParticipantController.cs
@@ -1,11 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#nullable enable
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rauthor.Models;
+using Rauthor.Services;
+using Rauthor.ViewModels.Api;
+using System;
+using System.Linq;
 
 namespace Rauthor.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ParticipantController
+    public class ParticipantController : ControllerBase
     {
+        DatabaseContext database;
 
+        public ParticipantController(DatabaseContext db)
+        {
+            database = db;
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPost]
+        public IActionResult Post([FromBody] ViewModels.Api.ParticipantPost value)
+        {
+            var competitionExists = database.Competitions.SingleOrDefault(x => x.Guid == value.Guid) != null;
+            if (competitionExists)
+            {
+                var participant = new Models.Participant()
+                {
+                    Guid = Guid.NewGuid(),
+                    CompetitionGuid = value.Guid,
+                    CreateDate = DateTime.Now,
+                    Nickname = value.Nickname,
+                    Status = Models.ParticipantStatus.New,
+                    UserGuid = HttpContext.Session.Get<User>("user").Guid,
+                };
+                var poem = new Poem()
+                {
+                    Guid = Guid.NewGuid(),
+                    ParticipantGuid = participant.Guid,
+                    Text = value.Poem,
+                    Title = value.Title
+                };
+                database.Database.BeginTransaction();
+                database.Participants.Add(participant);
+                database.Poems.Add(poem);
+                database.Database.CommitTransaction();
+                var result = new ParticipantPostResult() 
+                { 
+                    CreatedParticipantGuid = participant.Guid,
+                    PoemGuids = { poem.Guid }
+                };
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("Такого соревнования не существует.");
+            }
+        }
     }
 }
---
+++ Rauthor/ViewModels/Api/Participant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rauthor.ViewModels.Api
+{
+    public class ParticipantPost
+    {
+        /// <summary>
+        /// Guid соревнования, в котором собирается участвовать пользователь.
+        /// </summary>
+        public Guid Guid { get; set; }
+        /// <summary>
+        /// Название произведения.
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Псевдоним пользователя.
+        /// </summary>
+        public string Nickname { get; set; }
+        /// <summary>
+        /// Текст произведения.
+        /// </summary>
+        public string Poem { get; set; }
+    }
+
+    public class ParticipantPostResult
+    {
+        /// <summary>
+        /// Guid созданной заявки на участие в конкурсе.
+        /// </summary>
+        public Guid CreatedParticipantGuid { get; set; }
+        /// <summary>
+        /// Guid-ы произведений, связанных с этой заявкой.
+        /// </summary>
+        public List<Guid> PoemGuids { get; set; }
+    }
+}
