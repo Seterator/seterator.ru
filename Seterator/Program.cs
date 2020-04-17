611757dd6108cf75ea9649957efd249b02ebe4c2
Азамат Шаяхметов <shayakhmetov94@gmail.com>
2020-04-17 23:08:29 +0500

    Реализована интеграция NLog со стандартным механизмом логирования .NET Core. Добавлено логирование необработанных исключений.

--- Seterator/Program.cs
+++ Seterator/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace Seterator
 {
@@ -15,11 +11,26 @@ namespace Seterator
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("NLogger initialized");
+                CreateWebHostBuilder(args).Build().Run();
+            } catch(Exception e) {
+                logger.Error(e, "Unrecoverable exception while running the app");
+                throw;
+            } finally {
+                LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((loggerBuilder) => {
+                    loggerBuilder.ClearProviders();
+                    loggerBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                })
+                .UseNLog()
                 .UseKestrel()
                 .UseUrls("http://*:80/", "https://*:8080/", "https://*:5001/", "https://www.seterator.ru")
                 .UseStartup<Startup>();
