588711fc68f48fd184625c04b9a108a83344852b
Михаил Проказин <bearpro@outlook.com>
2020-06-05 00:34:41 +0300

    Причёсан код deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 echo "[*] Deployment started..."
-sshpass -e ssh -o StrictHostKeyChecking=no root@5.63.154.249 '
-    echo "[*] SSH session started..." &&
-    cd seterator &&
-    systemctl stop seterator &&
-    echo "[*] seterator.service stopped..." &&
-    git pull &&
-    echo "[*] Source code updated, building..." &&
-    dotnet build &&
-    echo "[*] Build completed, applying migration..." &&
-    dotnet ef database update --project ./Seterator/Seterator.csproj &&
-    echo "[*] Migration completed, starting seterator.service..."
-    systemctl start seterator &&
+sshpass -e ssh -o StrictHostKeyChecking=no root@45.151.144.11 '
+    echo "[*] SSH session started..."                                   &&
+    cd /var/seterator                                                   &&
+    echo "[*] Stopping seterator.service..."                            &&
+    systemctl stop seterator                                            &&
+    echo "[*] seterator.service stopped, updating source code..."       &&
+    git pull                                                            &&
+    echo "[*] Source code updated, building..."                         &&
+    dotnet build                                                        &&
+    echo "[*] Build completed, applying migration..."                   &&
+    dotnet ef database update --project ./Seterator/Seterator.csproj    &&
+    echo "[*] Migration applied"
+    echo "[*] starting seterator.service..."
+    systemctl start seterator                                           &&
     echo "[*] seterator.service started"
 '
-echo "[*] Deployment complited"
+echo "[*] Deployment complited!"
