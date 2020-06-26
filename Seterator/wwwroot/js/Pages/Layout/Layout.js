010cab52e200eda1197db904593ec3bcd37dd0be
semen1032 <51064226+semen1032@users.noreply.github.com>
2020-06-26 19:21:23 +0300

    Удален тестовый компонент в верху страницы

--- Seterator/wwwroot/js/Pages/Layout/Layout.js
+++ Seterator/wwwroot/js/Pages/Layout/Layout.js
@@ -1,16 +1,10 @@
 // Точка входа js для страницы макета
 import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import AppComponent from '../../../components/App.vue';
-import HeaderComponent from '../../../components/Header/Header.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import HeaderComponent from '../../../components/Header/Header.vue';
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
-new Vue({
-    el: '#el',
-    render: h => h(AppComponent)
-});
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
     el: '#header-component',
