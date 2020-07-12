d6f394a1f144c92746ec1ca7c6d93a537b21033d
semen1032 <51064226+semen1032@users.noreply.github.com>
2020-07-12 08:18:52 +0300

    Добавлены аватар и социальные сети

--- Seterator/wwwroot/components/Profile/Profile.vue
+++ Seterator/wwwroot/components/Profile/Profile.vue
@@ -1,15 +1,84 @@
 <template>
   <div class="container">
-      <h2>Работает</h2>
+      <div class="profilePersonal__wrap">
+        <div class="profilePersonal__leftSide">
+            <profile-avatar class="profile__avatar-wrap" src="/img/Profile/ava.png"></profile-avatar>
+            <social-icons class="profile__socialIcons" :socialIcons="socialIcons__data" :color="socialIcons__color"></social-icons>
+            <div class="profile__rating-wrap">
+                <h5>Рейтинг</h5>
+            </div>
+            <div class="profile__drafts-wrap">
+                <h5>Черновики</h5>
+            </div>
+        </div>
+        <div class="profilePersonal__rightSide">
+            <div class="profile__about-wrap">
+                <h5>Об авторе</h5>
+            </div>
+            <div class="profile__achievments-wrap">
+                <h5>Достижения</h5>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import ProfileAvatar from './ProfileAvatar.vue';
+import SocialIcons from '../Other/SocialIcons.vue';
 
+let s = [
+    {
+        name: 'vk',
+        href: 'https://vk.com/seterator',
+    },
+    {
+        name: 'telegram',
+        href: 'tg.com/seterator',
+    },
+    {
+        name: 'github',
+        href: 'https://github.com'
+    }
+];
+
+export default {
+    components: {
+        'profile-avatar': ProfileAvatar,
+        'social-icons': SocialIcons
+    },
+    data: function() {
+        return {
+            socialIcons__data: [
+                {
+                    name: 'vk',
+                    href: 'https://vk.com/seterator',
+                },
+                {
+                    name: 'telegram',
+                    href: 'tg.com/seterator',
+                },
+                {
+                    name: 'github',
+                    href: 'https://github.com'
+                }
+            ],
+            socialIcons__color: 'red'
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.profilePersonal__wrap {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    gap: 30px;
+}
 
+.profilePersonal__leftSide,
+.profilePersonal__rightSide {
+    display: flex;
+    flex-direction: column;
+}
 </style>
---
+++ Seterator/wwwroot/components/Profile/ProfileAvatar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="profileAvatar">
+        <img :src="src" alt="Аватар пользователя">
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        src: String
+    }
+}
+</script>
+
+<style>
+.profileAvatar {
+    max-width: 100%;
+}
+
+.profileAvatar img {
+    width: 100%;
+}
+</style>
