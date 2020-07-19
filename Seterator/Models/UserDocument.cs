7d3cdba156b8f6176fa6e301f942e16e278ed4b3
Азамат Шаяхметов <shayakhmetov94@gmail.com>
2020-07-19 19:35:39 +0500

    Revert "Revert "Добавлены общие поля пользователя, сгенерирована миграция.""
    This reverts commit b401e80937e935d38a7f32c12a51b21105de5755.

---
+++ Seterator/Models/UserDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seterator.Models
+{
+    [Table("Documents")]
+    public class UserDocument
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("OwnerGuid")]
+        public Guid OwnerGuid { get; set; }
+
+        [ForeignKey("OwnerGuid")]
+        public virtual User Owner{ get; set; }
+        
+        [Column("type")]
+        public string Type { get; set; }
+
+        [Column("data")]
+#pragma warning disable CA1819 // Properties should not return arrays
+        public byte[] Data { get; set; }
+#pragma warning restore CA1819 // Properties should not return arrays
+    }
+}
