d7c235650c9bd5921ced13f504ae3f9731ffd046
semen1032 <51064226+semen1032@users.noreply.github.com>
2020-09-16 19:14:48 +0300

    Код почищен от мусора

--- Seterator/wwwroot/js/account.js
+++
@@ -1,8 +0,0 @@
-async function login(username, password) {
-    let model = JSON.stringify({ "Username": username, "Password": password });
-    let response = fetch("api/account", {
-        body: model,
-        method: "post"
-    });
-    console.log(await response);
-}
